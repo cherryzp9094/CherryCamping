f979e419a4aaca919b483f7ec696c013782e002d
cherry <sklee@herren.co.kr>
2022-12-25 18:10:26 +0900

    feat: LiveData -> StateFlow로 변경한다

--- presentation/src/main/java/com/cherryzp/cherrycamping/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/cherryzp/cherrycamping/ui/main/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.cherryzp.cherrycamping.ui
+package com.cherryzp.cherrycamping.ui.main
 
-import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.cherryzp.cherrycamping.R
-import com.cherryzp.cherrycamping.base.BaseActivity
+import com.cherryzp.cherrycamping.ui.base.BaseActivity
 import com.cherryzp.cherrycamping.databinding.ActivityMainBinding
+import com.cherryzp.cherrycamping.extends.repeatOnStarted
+import com.cherryzp.cherrycamping.ui.CampingRecyclerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -20,15 +21,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private val campingAdapter = CampingRecyclerAdapter()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setupCampingItem()
-        initCampingRecyclerAdapter()
-
-        viewModel.getCampingPagingList()
-    }
-
     private fun initCampingRecyclerAdapter() {
         binding.rvCampingList.apply {
             layoutManager = GridLayoutManager(this@MainActivity, 2, GridLayoutManager.VERTICAL, false)
@@ -37,14 +29,26 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     private fun setupCampingItem() {
-        viewModel.state.observe(this) { state ->
-            when (state) {
-                is MainState.CampingData -> {
-                    lifecycleScope.launch {
-                        campingAdapter.submitData(state.item)
+        repeatOnStarted {
+            viewModel.eventFlow.collect { state ->
+                when (state) {
+                    is MainEvent.CampingData -> {
+                        lifecycleScope.launch {
+                            campingAdapter.submitData(state.item)
+                        }
                     }
+                    else -> {}
                 }
             }
         }
+    }
+
+    override fun beforeBinding() {
+        initCampingRecyclerAdapter()
+        setupCampingItem()
+    }
+
+    override fun afterBinding() {
+        viewModel.getCampingPagingList()
     }
 }
--- presentation/src/main/java/com/cherryzp/cherrycamping/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/cherryzp/cherrycamping/ui/main/MainViewModel.kt
@@ -1,26 +1,25 @@
-package com.cherryzp.cherrycamping.ui
+package com.cherryzp.cherrycamping.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
-import com.cherryzp.cherrycamping.base.BaseViewModel
+import com.cherryzp.cherrycamping.ui.base.BaseViewModel
 import com.cherryzp.domain.dto.CampingDto
 import com.cherryzp.domain.usecase.camping.GetCampingListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import com.cherryzp.domain.result.Result
-import com.cherryzp.domain.result.succeeded
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.*
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getCampingListUseCase: GetCampingListUseCase
 ): BaseViewModel() {
 
-    private val _state = MutableLiveData<MainState>()
-    val state: LiveData<MainState> get() = _state
+    private val _eventFlow = MutableStateFlow<MainEvent>(MainEvent.NONE)
+    val eventFlow: StateFlow<MainEvent> get() = _eventFlow
 
     private val _campingDtoList =  MutableLiveData<List<CampingDto>>()
     val campingDtoList: LiveData<List<CampingDto>>
@@ -31,7 +30,7 @@ class MainViewModel @Inject constructor(
             when(val response = getCampingListUseCase(20)) {
                 is Result.Success -> {
                     response.data.cachedIn(viewModelScope).collect {
-                        _state.value = MainState.CampingData(it)
+                        _eventFlow.value = MainEvent.CampingData(it)
                     }
                 }
                 else -> {
